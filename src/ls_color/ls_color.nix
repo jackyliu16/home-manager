e4a9ae0dd48b1da23af873245c799cddae1131e3
jackyliu <18922251299@163.com>
2023-02-21 14:22:45 +0800

    update(ls_color):
      1. remove the LS_COLORS file from the folder
      2. fetchurl from githubusercontent to get the file

--- src/ls_color/ls_color.nix
+++ src/ls_color/ls_color.nix
@@ -1,9 +1,13 @@
 let 
   pkgs = import <nixpkgs> {};
+  LS_COLORS = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/trapd00r/LS_COLORS/2baa6677b6d1491076059169d52b4021bd9be348/LS_COLORS";
+    sha256 = "sha256-U9ivaC2JbZ5wOwZyQbbSa3AZxDsGguifqjho6lTmC+Y=";
+  };
 in
   pkgs.runCommand "ls-colors" {} ''
     mkdir -p $out/bin $out/share
     ln -s ${pkgs.coreutils}/bin/ls          $out/bin/ls
     ln -s ${pkgs.coreutils}/bin/dircolors   $out/bin/dircolors
-    cp ${./LS_COLORS} $out/share/LS_COLORS
+    cp ${LS_COLORS} $out/share/LS_COLORS
   ''
