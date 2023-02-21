9f4a45e62ee9d004442b03eb7ba3eca9b9ebe780
jackyliu <18922251299@163.com>
2023-02-21 15:07:21 +0800

    refator: using fetchgit to replace fetchurl

--- src/ls_color.nix
+++ src/ls_color.nix
@@ -1,13 +1,18 @@
 let 
   pkgs = import <nixpkgs> {};
-  LS_COLORS = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/trapd00r/LS_COLORS/2baa6677b6d1491076059169d52b4021bd9be348/LS_COLORS";
-    sha256 = "sha256-U9ivaC2JbZ5wOwZyQbbSa3AZxDsGguifqjho6lTmC+Y=";
+  LS_COLORS = pkgs.fetchgit {
+    url = "https://github.com/trapd00r/LS_COLORS";
+    rev = "09dab448207002624d17f01ed7cbf820aa048063";
+    sha256 = "sha256-hQTT/yNS9UIDZqHuul0xmknnOh6tOtfotQIm0SY5TTE=";
   };
+  # LS_COLORS = pkgs.fetchurl {
+  #   url = "https://raw.githubusercontent.com/trapd00r/LS_COLORS/2baa6677b6d1491076059169d52b4021bd9be348/LS_COLORS";
+  #   sha256 = "sha256-U9ivaC2JbZ5wOwZyQbbSa3AZxDsGguifqjho6lTmC+Y=";
+  # };
 in
   pkgs.runCommand "ls-colors" {} ''
     mkdir -p $out/bin $out/share
     ln -s ${pkgs.coreutils}/bin/ls          $out/bin/ls
     ln -s ${pkgs.coreutils}/bin/dircolors   $out/bin/dircolors
-    cp ${LS_COLORS} $out/share/LS_COLORS
+    cp ${LS_COLORS}/LS_COLORS $out/share/LS_COLORS
   ''
