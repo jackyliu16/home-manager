670e1e714354cf17e9ef05d61bf1b36fd9b8bd0c
jackyliu16 <18922251299@163.com>
2023-02-24 14:33:07 +0800

    add ref on https://github.com/cmacrae/config/blob/master/modules/macintosh.nix fir zsh

--- home.nix
+++ home.nix
@@ -105,28 +105,86 @@ in
         theme = "base16-256";
       };
     };
+    # Ref on https://github.com/cmacrae/config/blob/master/modules/macintosh.nix
     zsh = {
       enable = true;
       enableAutosuggestions = true;
-      enableSyntaxHighlighting = true;
-      autocd = true;
-      defaultKeymap = "vicmd";
-      shellAliases = {
-        ll = "ls -l";
-        # ".." = "cd ..";
-        "hws" = "home-manager switch --flake ~/Documents/nix-config/";
-      };
-      # shellGlobalAliases = '''';
-      # .zshenv
-      envExtra = ''
-        export http_proxy=127.0.0.1:7890
-        export https_proxy=127.0.0.1:7890
+      enableCompletion = true;
+      sessionVariables = { RPROMPT = ""; };
+
+      # shellAliases = {
+      #   k = "kubectl";
+      #   kp = "kube-prompt";
+      #   kc = "kubectx";
+      #   kn = "kubens";
+      #   t = "cd $(mktemp -d)";
+      # };
+
+      oh-my-zsh.enable = true;
+
+      plugins = [
+        {
+          name = "autopair";
+          file = "autopair.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "hlissner";
+            repo = "zsh-autopair";
+            rev = "4039bf142ac6d264decc1eb7937a11b292e65e24";
+            sha256 = "02pf87aiyglwwg7asm8mnbf9b2bcm82pyi1cj50yj74z4kwil6d1";
+          };
+        }
+        {
+          name = "fast-syntax-highlighting";
+          file = "fast-syntax-highlighting.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "zdharma";
+            repo = "fast-syntax-highlighting";
+            rev = "v1.28";
+            sha256 = "106s7k9n7ssmgybh0kvdb8359f3rz60gfvxjxnxb4fg5gf1fs088";
+          };
+        }
+        {
+          name = "z";
+          file = "zsh-z.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "agkozak";
+            repo = "zsh-z";
+            rev = "41439755cf06f35e8bee8dffe04f728384905077";
+            sha256 = "1dzxbcif9q5m5zx3gvrhrfmkxspzf7b81k837gdb93c4aasgh6x6";
+          };
+        }
+      ];
+
+      initExtra = ''
+          PROMPT='%{$fg_bold[blue]%}$(get_pwd)%{$reset_color%} ''${prompt_suffix}'
+          local prompt_suffix="%(?:%{$fg_bold[green]%}❯ :%{$fg_bold[red]%}❯%{$reset_color%} "
+          function get_pwd(){
+              git_root=$PWD
+              while [[ $git_root != / && ! -e $git_root/.git ]]; do
+                  git_root=$git_root:h
+              done
+              if [[ $git_root = / ]]; then
+                  unset git_root
+                  prompt_short_dir=%~
+              else
+                  parent=''${git_root%\/*}
+                  prompt_short_dir=''${PWD#$parent/}
+              fi
+              echo $prompt_short_dir
+          }
+          vterm_printf(){
+              if [ -n "$TMUX" ]; then
+                  # Tell tmux to pass the escape sequences through
+                  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+                  printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+              elif [ "''${TERM%%-*}" = "screen" ]; then
+                  # GNU screen (screen, screen-256color, screen-256color-bce)
+                  printf "\eP\e]%s\007\e\\" "$1"
+              else
+                  printf "\e]%s\e\\" "$1"
+              fi
+        }
       '';
-      # .zshrc
-      # initExtra   
-      oh-my-zsh = {
-        enable = true;
-      };
     };
   };
 }
