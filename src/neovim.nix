5a3bf8069f31c6035e0b8a848b7298d2b0918d9d
jackyliu16 <18922251299@163.com>
2023-03-17 20:54:23 +0800

    update(mynixvim): copy my extra plugins

--- src/neovim.nix
+++ src/neovim.nix
@@ -35,5 +35,42 @@ in
 
       background = "dark";
     };
+    extraPlugins = with pkgs.vimPlugins; [
+      # vim-wakatime
+      # language support
+      # vim-lsp
+      # # asyncomplete-vim
+      # # jedi-vim
+      # vim-nix
+      # rust-vim
+      # vim-toml
+      # YouCompleteMe   # 自动补全
+
+      # UI
+      vim-gitgutter   # status in gitter
+      vim-airline     # vim-devicons
+      (nvim-treesitter.withPlugins (p: [ p.c p.java p.rust p.python p.go ]))
+      vim-bufferline  # 标签页
+      nvim-gdb
+      
+      ## colocscheme
+      # gruvbox
+      # vim-devicons    
+
+      # Tools
+      # auto-pairs
+      # LeaderF         # 模糊查找
+      # nerdcommenter   # 多行注释支持
+      # Vundle-vim      # plug-in manager for Vim
+      # # lightline     # tabline customization
+      # vim-startify    # 最近打开的文件
+      # vim-fugitive    # Git Support
+
+      # NERDTree
+      nerdtree
+      vim-nerdtree-tabs
+      vim-nerdtree-syntax-highlight
+      nerdtree-git-plugin
+    ];
   };
 }
