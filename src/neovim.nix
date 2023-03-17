5797fe7ef1690a582356d80d7fb834d42b014134
jackyliu16 <18922251299@163.com>
2023-03-17 20:42:33 +0800

    update(mynixvim): finish the basic copy
      1. copy my vim configuration to nixvim

--- src/neovim.nix
+++ src/neovim.nix
@@ -10,10 +10,31 @@ in
     nixvim.homeManagerModules.nixvim
     # For NixOS nixvim.nixosModules.nixvim
     # For nix-darwin nixvim.nixDarwinModules.nixvim
-    
-  
-
   ];
 
-  programs.nixvim.enable = true;
+  programs.nixvim = {
+    enable = true;
+    vialias = true;
+    vimAlias = true;
+
+    options = {
+      mouse = "a";
+      spelllang = "en_us";
+      clipboard = "unnamedplus";
+
+      syntax = true;
+      wrap = true;
+      smartindent = true;
+
+      number = true;
+      relativenumber = true;
+      expandtab = true;
+      copyindent = true;
+
+      shiftwidth = 2;
+      tabstop = 2;
+
+      background = "dark";
+    };
+  };
 }
