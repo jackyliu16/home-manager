f95f16f849354fa0d04965287f8ace839c12f074
jackyliu <18922251299@163.com>
2023-03-18 11:25:16 +0800

    update(home): add sessionVariables
    based on https://nixos.wiki/wiki/Environment_variables

--- home.nix
+++ home.nix
@@ -28,6 +28,16 @@ in
   # paths it should manage.
   home.username = "${user}";
   home.homeDirectory = "/home/${user}";
+  home.sessionVariables = rec {
+    XDG_CACHE_HOME  = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    XDG_BIN_HOME    = "\${HOME}/.local/bin";
+    XDG_DATA_HOME   = "\${HOME}/.local/share";
+
+    PATH = [ 
+      "\${XDG_BIN_HOME}"
+    ];
+  };
   home.stateVersion = "22.11";
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
