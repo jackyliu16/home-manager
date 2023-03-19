d3902aaf9a924b149c7014e3abff164074fe1759
jackyliu <18922251299@163.com>
2023-03-19 17:54:02 +0800

    update(mynixvim): change extraConfig into nixvim

--- vimAndNeovim/autocomplete.nix
+++ vimAndNeovim/autocomplete.nix
@@ -21,6 +21,36 @@
           throttle = 30;
           fetchingTimeout = 500;
         };
+        mapping = {
+          # prev page
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          # next page
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          # start complete
+          "<C-Space>" = "cmp.mapping.complete()";
+          # close complete
+          "<C-e>" = "cmp.mapping.close()";
+          # next item
+          "<Tab>" = {
+            modes = ["i" "s"];
+            action = "cmp.mapping.select_next_item()";
+          };
+          # prev item
+          "<S-Tab>" = {
+            modes = ["i" "s"];
+            action = "cmp.mapping.select_prev_item()";
+          };
+          # confirm to using a specify complete item
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
+        sources = [
+          { name = "path"; }
+          { name = "nvim_lsp"; }
+          { name = "vsnip"; }
+          { name = "buffer"; }
+          # { name = "neorg"; }
+          # { name = "cmp_tabnine"; }
+        ];
       };    
       cmp-vsnip.enable    = true;
     };
@@ -31,39 +61,6 @@
           -- 设置代码片段引擎，用于根据代码片段补全
           window = {
           },
-
-          mapping = {
-              -- 选择上一个
-              ['<C-p>'] = cmp.mapping.select_prev_item(),
-              -- 选择下一个
-              ['<Tab>'] = cmp.mapping.select_next_item(),
-              -- 出现补全
-              ['<A-.>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
-              -- 取消补全
-              ['<A-,>'] = cmp.mapping({
-                  i = cmp.mapping.abort(),
-                  c = cmp.mapping.close(),
-              }),
-
-              -- 确认使用某个补全项
-              ['<CR>'] = cmp.mapping.confirm({
-                  select = true,
-                  behavior = cmp.ConfirmBehavior.Replace
-              }),
-
-              -- 向上翻页
-              ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-              -- 向下翻页
-              ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-          },
-
-          -- 补全来源
-          sources = cmp.config.sources({
-              {name = 'nvim_lsp'},
-              {name = 'vsnip'},
-              {name = 'buffer'},
-              {name = 'path'}
-          }),
 
           --根据文件类型来选择补全来源
           cmp.setup.filetype('gitcommit', {
