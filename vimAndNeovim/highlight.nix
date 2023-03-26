bb5c20c8d8e9f2f7c878ef37c4904782b4bc20e3
jackyliu <18922251299@163.com>
2023-03-26 19:45:42 +0800

    refator(mynivim:startify) move to-comments to highlight file

--- vimAndNeovim/highlight.nix
+++ vimAndNeovim/highlight.nix
@@ -1,21 +1,32 @@
 { 
   programs.nixvim = {
+    plugins.todo-comments = {
+      enable = true;
+      colors = {
+        error = [ "DiagnosticError" "ErrorMsg" "#DC2626" ];
+        warning = [ "DiagnosticWarn" "WarningMsg" "#FBBF24" ];
+        info = [ "DiagnosticInfo" "#2563EB" ];
+        hint = [ "DiagnosticHint" "#10B981" ];
+        default = [ "Identifier" "#7C3AED" ];
+        test = [ "Identifier" "#FF00FF" ];
+      };
+    };
     highlight.Todo = {
       fg = "Blue";
       bg = "Yellow";
     };
-    match.TODO = "TODO";
-    maps.normal."<C-t>" = {
-      action = ''
-        function()
-          require('telescope.builtin').live_grep({
-            default_text="TODO",
-            initial_mode="normal"
-          })
-        end
-      '';
-      lua = true;
-      silent = true;
-    };
+    # match.TODO = "TODO";
+    # maps.normal."<C-t>" = {
+    #   action = ''
+    #     function()
+    #       require('telescope.builtin').live_grep({
+    #         default_text="TODO",
+    #         initial_mode="normal"
+    #       })
+    #     end
+    #   '';
+    #   lua = true;
+    #   silent = true;
+    # };
   };
 }
