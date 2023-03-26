238d0a75230d746b65ceba212bbc3fc0933980ff
jackyliu <18922251299@163.com>
2023-03-26 19:10:53 +0800

    refator(mynivim:startify) move to single file

--- vimAndNeovim/nixvim.nix
+++ vimAndNeovim/nixvim.nix
@@ -97,25 +97,6 @@ in
           rust
         ];
       };
-      startify = {
-        enable = true;
-        bookmarks = [
-          { c="~/.config/nixpkgs/"; }
-          { ch= "~/.config/nixpkgs/home.nix"; }
-        ];
-        # https://www.ascii-art-generator.org/
-        customHeader = [
-"    ____ _     _  __   __            ____  _             _         _   _               _   _____         _            ___  "
-"   |  _ \\(_) __| | \\ \\ / /__  _   _  / ___|| |_ _   _  __| |_   _  | | | | __ _ _ __ __| | |_   _|__   __| | __ _ _   |__ \\ "
-"   | | | | |/ _` |  \\ V / _ \\| | | | \\___ \\| __| | | |/ _` | | | | | |_| |/ _` | '__/ _` |   | |/ _ \\ / _` |/ _` | | | |/ / "
-"   | |_| | | (_| |   | | (_) | |_| |  ___) | |_| |_| | (_| | |_| | |  _  | (_| | | | (_| |   | | (_) | (_| | (_| | |_| |_|  "
-"   |____/|_|\\__,_|   |_|\\___/ \\__,_| |____/ \\__|\\__,_|\\__,_|\\__, | |_| |_|\\__,_|_|  \\__,_|   |_|\\___/ \\__,_|\\__,_|\\__, (_)  "
-"                                                            |___/                                                 |___/     " 
-        ];
-        changeToDir = true;
-        changeToVcsRoot = true; # When opening a file or bookmark, change to the root directory of the VCS
-        sessionAutoload = true;
-      };         # 新标签页
       # change in different buffer
       bufferline = {
         enable = true;
