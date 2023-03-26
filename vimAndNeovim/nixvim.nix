85c9be0b042aaf4ba387240b9ed85232395825ee
jackyliu <18922251299@163.com>
2023-03-26 14:46:19 +0800

    update(mynixvim): add options

--- vimAndNeovim/nixvim.nix
+++ vimAndNeovim/nixvim.nix
@@ -127,6 +127,32 @@ in
         "utf-8"
         "chinese"
       ];
+      # splitbelow = true; # A new window is put below the current one
+      # splitright = true; # A new window is put right of the current one
+
+      swapfile = false; # Disable the swap file
+      # modeline = true; # Tags such as 'vim:ft=sh'
+      # modelines = 100; # Sets the type of modelines
+      # undofile = true; # Automatically save and restore undo history
+      incsearch = true; # Incremental search: show match for partly typed search command
+      ignorecase = true; # When the search query is lower-case, match both lower and upper-case
+      #   patterns
+      # smartcase = true; # Override the 'ignorecase' option if the search pattern contains upper
+      # #   case characters
+      # scrolloff = 8; # Number of screen lines to show around the cursor
+      # cursorline = false; # Highlight the screen line of the cursor
+      # cursorcolumn = false; # Highlight the screen column of the cursor
+      # signcolumn = "yes"; # Whether to show the signcolumn
+      # colorcolumn = "100"; # Columns to highlight
+      # laststatus = 3; # When to use a status line for the last window
+      # termguicolors = true; # Enables 24-bit RGB color in the |TUI|
+      # spell = false; # Highlight spelling mistakes (local to window)
+
+      # textwidth = 0; # Maximum width of text that is being inserted.  A longer line will be
+      # #   broken after white space to get this width.
+
+      # # Folding
+      # foldlevel = 99; # Folds with a level higher than this number will be closed
     };
 
     extraPlugins = with pkgs.vimPlugins; [
